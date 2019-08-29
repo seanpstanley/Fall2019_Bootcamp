6a56df7c5c45a4686a8ba1c71adab0c634194590
Sean Stanley <spstanley@ufl.edu>
2019-08-29 12:24:54 -0400

    Finished Bootcamp Assignment 1

--- server.js
+++ server.js
@@ -7,45 +7,34 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+    // parse the url to find the path
+    var parsedUrl = url.parse(request.url);
     
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+    // if the path of the url is /listings and request type is get, send JSON
+    if (request.method == 'GET' && parsedUrl.pathname == '/listings') {
+        // specify the content type and send the listing data
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(listingData);
+
+    }
+    // if the path of the url is not /listings and the request type is not get, send 404 error
+    else {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end('Bad gateway error'); 
+    }  
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+    if (err) throw err;
+    // grab the JSON info from the file
+    listingData = data;
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+    // creates (not starts) server
+    server = http.createServer(requestHandler);
 
-    HINT: Read up on JSON parsing Node.js
-   */
-
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
-
-
+    // stars server and listens for requests on port 8080
+    server.listen(port, function() {
+        // executes callback function after server is listening
+        console.log('Server listening on: http://127.0.0.1:' + port);
+    });
 });
