216eac6c6583de069c98fd38f4e8fdbbbac349c6
seanpstanley <spstanley@ufl.edu>
2019-09-15 20:17:13 -0400

    Delete config file to be submitted separately

--- config.js
+++
@@ -1,9 +0,0 @@
-//This file holds any configuration variables we may need 
-//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
-//copy this file's contents to another file 'config.js' and store your MongoLab uri there
-
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://spstanley:tGlwCThtNUSnMDME@cluster0-a2efp.mongodb.net/Bootcamp2?retryWrites=true&w=majority', //place the URI of your mongo database here.
-  }
-};
